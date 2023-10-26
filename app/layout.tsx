175dcb0a1557258645a399038fea953cd1b8a82e
Sidharth Sethi <sidharth@figr.design>
2023-10-27 02:38:57 +0530

    Added Assests

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,31 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
 import React from 'react';
+
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
+
+export const metadata: Metadata = {
+  title: 'Doubt Sync',
+  description:
+    'A place for students to ask questions and get answers from their peers developers.',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,10 +33,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient',
+          hover: 'text-primary-500',
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <h1 className="h1-bold">Hello World</h1>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
 }
+
