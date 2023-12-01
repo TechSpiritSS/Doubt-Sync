67c5d88acd021806f83b0df06270fef2cf585c83
Sidharth Sethi <sidharth@figr.design>
2023-12-02 02:16:31 +0530

    Added Tags

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -4,9 +4,11 @@ import { Input } from '@/components/ui/input';
 import { QuestionsSchema } from '@/lib/validations';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Editor } from '@tinymce/tinymce-react';
-import { useRef } from 'react';
+import Image from 'next/image';
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { Badge } from '../ui/badge';
 import { Button } from '../ui/button';
 import {
   Form,
@@ -33,6 +35,39 @@ const Question = () => {
   function onSubmit(data: z.infer<typeof QuestionsSchema>) {
     console.log(data);
   }
+
+  const handleInputKetDown = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) => {
+    if (e.key === 'Enter' && field.name === 'tags') {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const value = tagInput.value.trim();
+
+      if (value !== '') {
+        if (value.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters in size',
+          });
+        }
+
+        if (!field.value.includes(value as never)) {
+          form.setValue('tags', [...field.value, value]);
+          tagInput.value = '';
+          form.clearErrors('tags');
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  };
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const tags = field.value.filter((t: string) => t !== tag);
+    form.setValue('tags', tags);
+  };
 
   return (
     <Form {...form}>
@@ -126,11 +161,38 @@ const Question = () => {
                 Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  {...field}
-                  className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
-                  placeholder="Add Tags..."
-                />
+                <>
+                  <Input
+                    onKeyDown={(e) => {
+                      handleInputKetDown(e, field);
+                    }}
+                    className="no-focus paragraph-regular background-light900_dark300 text-dark300_light700 light-border-2 min-h-[56px] border"
+                    placeholder="Add Tags..."
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => {
+                            handleTagRemove(tag, field);
+                          }}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close Icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
 
               <FormDescription className="body-regular mt-2.5 text-light-500">
