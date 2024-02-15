446b2869825d47174952534698cbf9d6c1a0260a
Sidharth Sethi <Sidharth.sherry@gmail.com>
2024-02-16 00:04:41 +0530

    clerk images

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
     serverActions: true,
     serverComponentsExternalPackages: ['mongoose'],
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagsapi.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
